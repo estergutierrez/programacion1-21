d5dcebd1c0d1b3b42c9e407e3701b7e75e41694e
Jose Antonio Ciccio <jciccio@gmail.com>
2021-05-11 06:19:25 -0600

    .

--- Arreglo.java
+++ Arreglo.java
@@ -1,12 +1,5 @@
 public class Arreglo{
 	public static void main (String[] args){
-		int [] datos = new int [10];
-		for (int i = 0 ; i < 10 ; i++){
-			datos[i] = i+1;
-		}
-
-		for (int celda : datos){
-			System.out.println(celda);
-		}
+		
 	}
 }
