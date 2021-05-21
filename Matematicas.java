02c626d653a3bb5b185ead29750bb857c31a544c
Jose Antonio Ciccio <jciccio@gmail.com>
2021-05-21 09:18:38 -0600

    Ejemplo matrices

--- Matematicas.java
+++ Matematicas.java
@@ -1,9 +1,46 @@
 public class Matematicas {
 	
+	// 2 -> 1 2
+	// 5 -> 1 5
+	// 10 -> 1 10 (2 5)
+
+	public boolean esPrimo(int numero){
+		// Dividir consecutivamente
+		// Si el numero es divisor invalida que sea primo
+		// Sacar los pares de una vez -> Optimización
+		boolean primo = (numero >= 2);
+		int contador = 2;
+		while(primo && contador <= numero/2){
+			if(numero%contador == 0){
+				primo = false;
+			}
+			contador++;
+		}
+		return primo;
+	}
+
+	public void imprimirPrimos(){
+		for(int i = 2; i < 1000; i++){
+			if (this.esPrimo(i)){
+				System.out.println("El número "+ i +" es primo");
+			}
+		}
+	}
+
+
+
+
 
 	public static void main (String [] args){
 		Matematicas matematicas = new Matematicas();
 		System.out.println(matematicas.esPrimo(10));
+		System.out.println(matematicas.esPrimo(100));
+		System.out.println(matematicas.esPrimo(27));
+		System.out.println(matematicas.esPrimo(5));
+		System.out.println(matematicas.esPrimo(23));
+		System.out.println(matematicas.esPrimo(-23));
+
+		matematicas.imprimirPrimos();
 	}
 
 
---
+++ PracticaMatrices.java
@@ -0,0 +1,46 @@
+public class PracticaMatrices{
+	
+/*
+	Cree un método llamado sumarMatrices que intentará sumar 
+	una matrizA con una matrizB. Tome 
+	en cuenta que debe validar los tamaños de las matrices, 
+	en caso contrario no se podrán sumar y 
+	retornará los resultados. (Sumar matrices quiere decir sumar 
+	cada entrada de la matrizA con su 
+	entrada correspondiente en la matrizB)
+*/
+
+	public int [][] sumarMatrices(int [][] matrizA, int [][] matrizB){
+		int [][] resultado = null;
+		if(esRectangular(matrizA) && esRectangular(matrizB) &&
+		   matrizA.length == matrizB.length && 
+		   matrizA[0].length == matrizB[0].length){
+
+			resultado = new int [matrizA.length][matrizA[0].length];
+			for(int f = 0; f < resultado.length; f++){
+				for(int c = 0; c < resultado[f].length; c++){
+					resultado[f][c] = matrizA[f][c] + matrizB[f][c];
+				}
+			}
+		}
+		return resultado;
+	}
+
+	public boolean esRectangular(int [][] matriz){
+		boolean resultado = true;
+		int tam = matriz[0].length;
+		for (int f = 0; f < matriz.length; f++){
+			if(tam != matriz[f].length){
+				resultado = false;
+			}
+		}
+		return resultado;
+	}
+
+	public static void main (String [] args){
+		PracticaMatrices pm = new PracticaMatrices();
+		int [][] matrizA = {{1,2,3},{4,5,6}};
+		int [][] matrizB = {{6,5,4},{9,8,7}};
+	}
+
+}
